c0dfcecd8763eb6c78e0c056bd8679c5cc7b2c5a
the-electro-punk <bwr2555@hotmail.com>
2021-09-14 20:06:00 -0500

    added button click listener

--- assets/js/Calender.js
+++ assets/js/Calender.js
@@ -2,19 +2,38 @@ var today = new Date()
 // for some reason, the day was 12 days behind so I added 12 to correct it
 var date = (today.getMonth()+1)+'/'+(today.getDay()+12)+'/'+today.getFullYear()
 console.log(date)
+
 var hour = today.getHours()
 // +':'+today.getMinutes()
 console.log("current hours is " + hour)
+
 let currentDate = document.getElementById("currentDate")
 
+const addEvent = document.getElementById('add-event')
+const deleteEvent = document.getElementById('delete-event')
+
+addEvent.addEventListener('click', oneMoreEvent)
+
+function oneMoreEvent() {
+    let inputForm = document.createElement('form')
+    inputForm.setAttribute()
+}
+
+// I created seperate IDs for each timeblock so I could more easily alter their colors
 const sevenAM = document.getElementById("7AM") 
 const eightAM = document.getElementById("8AM") 
+const nineAM = document.getElementById("9AM")
+const tenAM = document.getElementById("10AM")
+const elevenAM = document.getElementById("11AM")
 
 // this fills the current date at the top
+// it also ensures each timeblock starts as the right color
 function getCurrentDate() {
     currentDate.textContent = date
     sevenAM.style.background = "aqua"
     eightAM.style.background = "aqua"
+    nineAM.style.background = "aqua"
+    tenAM.style.background = "aqua"
 }
 getCurrentDate()
 
@@ -28,6 +47,15 @@ function timeColors() {
     }
     if (hour >= 9) {
         eightAM.style.background = "darkgray"
+    }
+    if (hour >= 10) {
+        nineAM.style.background = "darkgray"
+    }
+    if (hour >= 11) {
+        tenAM.style.background = "darkgray"
+    }
+    if (hour >= 12) {
+        elevenAM.style.background = "darkgray"
     }
 }
 
